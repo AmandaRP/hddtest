18db8ed7a1331fa952d41c83cd52aba451d96948
AmandaRP <25732302+AmandaRP@users.noreply.github.com>
2018-06-18 01:01:19 +0000

    Added package help documentation.

---
+++ R/hddtest.R
@@ -0,0 +1,10 @@
+#' hddtest: A package for testing two multinomial datasets.
+#'
+#' The hdd test package provides the following functions:
+#' multinom.test, etc, etc.
+#'
+#' @section hddtest functions:
+#'
+#' @docType package
+#' @name hddtest
+NULL
