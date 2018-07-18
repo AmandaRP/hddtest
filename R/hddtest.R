182d7b745b0987d9d6a99c0baa14f20dc1c412bf
AmandaRP <25732302+AmandaRP@users.noreply.github.com>
2018-07-18 02:38:36 +0000

    Updated documentation.

--- R/hddtest.R
+++ R/hddtest.R
@@ -1,10 +1,19 @@
-#' hddtest: A package for testing two multinomial datasets.
+#' hddtest: Two-sample hypothesis testing for high dimensional discrete disributions
 #'
-#' The hdd test package provides the following functions:
-#' multinom.test, genMultinomialData, etc.
+#' The hdd test package provides functionality for two-sample hypothesis testing of high dimensional discrete data. Specifically, testing of multinomial and multivariate binary data are supported.
 #'
 #' @section hddtest functions:
+#' multinom.test,
+#' genMultinomialData,
 #'
+#' @section Authors:
+#' Amanda Plunkett, Junyong Park
+#'
+#' @section References:
+#' \itemize{
+#' \item Amanda Plunkett & Junyong Park (2018) \emph{Two-Sample Test for Sparse High Dimensional Multinomial Distributions}, \url{arxiv.org/abs/1711.05524}
+#' \item Amanda Plunkett & Junyong Park (2017) \emph{Two-sample tests for sparse high-dimensional binary data}, Communications in Statistics - Theory and Methods, 46:22, 11181-11193
+#' }
 #' @docType package
 #' @name hddtest
 NULL
